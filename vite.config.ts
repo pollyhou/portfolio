5f5c880eece69789779280c9fec76440c2465d25
pollyhou <pollyhou123@gmail.com>
2026-03-27 00:11:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path';
 import { defineConfig } from '@lark-apaas/fullstack-vite-preset';
 
 export default defineConfig({
+  base: '/portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
